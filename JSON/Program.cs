5d319ab930952acb7bea74d873fb2b5f5edb77ce
Kilsii <kilzinkil08@gmail.com>
2026-05-29 11:15:48 +0300

    Task 1:Create JSON file and deserialize single user

---
+++ JSON/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+class Program
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+        string filePath = "users.json";
+        string jsonData = File.ReadAllText(filePath);
+        User user = JsonConvert.DeserializeObject<User>(jsonData);
+
+        Console.WriteLine($"Name: {user.Name}");
+        Console.WriteLine($"Age: {user.Age}");
+        Console.WriteLine($"City: {user.City}");
+    }
+}
