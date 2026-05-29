6f6d27bc3409f1e27a9b910d6423bc7202960bca
Kilsii <kilzinkil08@gmail.com>
2026-05-29 11:55:16 +0300

    Task 4: Inheritance -Admin and RegularUser extend User Class

--- JSON/Program.cs
+++ JSON/Program.cs
@@ -12,18 +12,24 @@ class Program
         public string City { get; set; }
     }
 
+    public class Admin : User
+    {
+        public string AccessLevel { get; set; }
+    }
+
+    
+    public class RegularUser : User
+    {
+        public string Subscription { get; set; }
+    }
+
     static void Main(string[] args)
     {
-        string filePath = "users.json";
-        string jsonData = File.ReadAllText(filePath);
+       
+        Admin admin = new Admin { Name = "Kilsi", Age = 18, City = "Siaulai", AccessLevel = "Admin" };
+        RegularUser regular = new RegularUser { Name = "Sarah", Age = 15, City = "Porthara", Subscription = "Free" };
 
-        // Deserialize into a list
-        List<User> users = JsonConvert.DeserializeObject<List<User>>(jsonData);
-
-        // Loop through and print each user
-        foreach (User user in users)
-        {
-            Console.WriteLine($"Name: {user.Name}, Age: {user.Age}, City: {user.City}");
-        }
+        Console.WriteLine($"Admin - Name: {admin.Name}, Age: {admin.Age}, City: {admin.City}, Access: {admin.AccessLevel}");
+        Console.WriteLine($"User - Name: {regular.Name}, Age: {regular.Age}, City: {regular.City}, Subscription: {regular.Subscription}");
     }
 }
